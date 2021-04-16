8ad8df644c95c7857215d4e6a6a1c176228c87e4
rajatguptaTechlib <rajatinvoice@gmail.com>
2021-04-16 19:21:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TelnyxRTC } from '@telnyx/webrtc';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Telnyx';
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+     this.http.get("http://jsonplaceholder.typicode.com/users").subscribe((d)=>{
+       console.log(d)
+     })
+  }
+
+  onSubmit(data:any){
+    console.warn(data);
+    if(data.SentToMsg == '' && data.SentTo == ''){
+      alert("Enter Valid Data")
+      return;
+    }
+    const headers = { 'Authorization': 'Bearer KEY017869E3526CCFDE1219DD7E9426565D_1gagsnapOuGviA2Cn79PB5'};
+    const body = {
+      "from": "+917971279011",
+      "to": data.SentTo,
+      "text": data.SentToMsg,
+      "media_urls" : []
+    }
+    this.http.post("https://api.telnyx.com/v2/messages",body,{headers}).subscribe((d)=>{
+       console.log(d)
+     })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TelnyxRTC } from '@telnyx/webrtc';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
